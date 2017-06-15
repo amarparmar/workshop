3496ee1920e219b9b111631e4b79950b364a38e6
amarparmar <amarsp666@gmail.com>
2017-06-14 23:02:49 -0400

    First Commit

---
+++ helloWorldProj/src/main/java/helloWorldProj/helloWorldProj/App.java
@@ -0,0 +1,13 @@
+package helloWorldProj.helloWorldProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World 1 2 3 4 5 6 7 " );
+    }
+}
